0533163e020bc240de46fa6d55ff7983302e0e4d
anchor200 <anchor200km@gmail.com>
2024-02-26 17:55:11 +0900

    swal-css

---
+++ swal.js
@@ -0,0 +1,13 @@
+function basicSample(){
+  swal("きをつけてね。");
+}
+
+function amazingSample() {
+ Swal.fire({
+     title: "",
+     html: '<iframe id="swal-demo" src=' + "https://okayama-kenbi.info/topi-hakujissyuu2024/" + '></iframe>',
+     showCloseButton: true,
+     customClass: 'swal-wide'
+ });
+}
+
